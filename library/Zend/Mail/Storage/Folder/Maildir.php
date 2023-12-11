00866a8acca74d5063f0a95218d3e89d27aefcc6
heguangyu5 <heguangyu5@qq.com>
2023-12-11 01:28:33 +0000

    代码优化 参数传多了

--- library/Zend/Mail/Storage/Folder/Maildir.php
+++ library/Zend/Mail/Storage/Folder/Maildir.php
@@ -242,7 +242,7 @@ class Zend_Mail_Storage_Folder_Maildir extends Zend_Mail_Storage_Maildir impleme
                 throw new Zend_Mail_Storage_Exception("{$this->_currentFolder} is not selectable", 0, $e);
             }
             // seems like file has vanished; rebuilding folder tree - but it's still an exception
-            $this->_buildFolderTree($this->_rootdir);
+            $this->_buildFolderTree();
             /**
              * @see Zend_Mail_Storage_Exception
              */
